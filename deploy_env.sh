daf978cf38c3878b44dab094eafd072211fab56c
Timur Nurlygayanov <tnurlygayanov@mirantis.com>
2015-12-11 17:33:27 +0300

    Updated script

--- deploy_env.sh
+++ deploy_env.sh
@@ -27,7 +27,7 @@ sudo chmod 777 /qa_environments/fuel-devops-venv
 virtualenv --system-site-packages /qa_environments/fuel-devops-venv
 
 source /qa_environments/fuel-devops-venv/bin/activate
-pip install git+https://github.com/openstack/fuel-devops.git@2.9.13 --upgrade
+pip install git+https://github.com/openstack/fuel-devops.git@2.9.10 --upgrade
 
 sudo virsh pool-define-as --type=dir --name=default --target=/var/lib/libvirt/images
 sudo virsh pool-autostart default
@@ -37,9 +37,11 @@ sudo usermod $(whoami) -a -G libvirtd,sudo
 
 sudo sed -ir 's/peer/trust/' /etc/postgresql/9.*/main/pg_hba.conf
 sudo service postgresql restart
-sudo expect -c 'spawn sudo -u postgres createuser -P fuel_devops; expect "password" {send -- "fuel_devops\rfuel_devops\r"};'
 
-#sudo -u postgres createuser -P fuel_devops
+sudo expect -c 'spawn sudo -u postgres createuser -P fuel_devops; expect "password" {send -- "fuel_devops\rfuel_devops\r"};'
 sudo -u postgres createdb fuel_devops -O fuel_devops
+
 django-admin.py syncdb --settings=devops.settings
 django-admin.py migrate devops --settings=devops.settings
+
+pip install git+https://github.com/openstack/fuel-qa.git --upgrade
