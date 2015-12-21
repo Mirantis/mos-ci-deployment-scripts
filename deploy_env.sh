f5c710f2ad332b07f333c8e953c3506caebeb4ef
Timur Nurlygayanov <tnurlygayanov@mirantis.com>
2015-12-21 15:31:51 +0300

    Added workaround

--- deploy_env.sh
+++ deploy_env.sh
@@ -63,6 +63,8 @@ git clone https://github.com/openstack/fuel-qa
 cp mos_tests.yaml fuel-qa/system_test/tests_templates/devops_configs/
 cp 3_controllers_2compute_neutronVLAN_and_ceph_env.yaml fuel-qa/system_test/tests_templates/tests_configs
 cd fuel-qa
+# the last working code:
+git checkout 16c327382babce82d4de2498040010c0f3c279fd
 sudo pip install -r fuelweb_test/requirements.txt
 
 # create new environment
