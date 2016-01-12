ea17ed1636407946ec579172916230b5b6bb4bc0
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-01-12 15:24:24 +0300

    Separated prepare script, yaml config fixes.

--- deploy_env.sh
+++ deploy_env.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # This script allows to deploy OpenStack environments
 # using simple configuration file
+
+# Hide trace on jenkins
+if [ -z "$JOB_NAME" ]; then
+    set -o xtrace
+fi
 
 if [ -z "$ISO_PATH" ]
 then
@@ -8,7 +13,7 @@ then
     exit 1
 fi
 
-if [ -z "$NEUTRONCONF" ]
+if [ -z "$NEUTRONCONF" ] || [ "$NEUTRONCONF" != 'VLAN' -a  "$NEUTRONCONF" != 'VxLAN' ]
 then
     echo "Please define env variable NEUTRONCONF as 'VLAN' or 'VxLAN'"
     exit 1
@@ -18,98 +23,77 @@ fi
 # https://github.com/openstack/fuel-devops/releases
 if [ -z "$FUEL_DEV_VER" ]
 then
-    fuel_devops_ver='2.9.15'
-else
-    fuel_devops_ver=$FUEL_DEV_VER
+    FUEL_DEV_VER='2.9.15'
 fi
 
 # Set fuel QA version
 # https://github.com/openstack/fuel-qa/branches
 if [ -z "$FUEL_QA_VER" ]
 then
-    fuel_qa_ver='master'
-else
-    fuel_qa_ver=$FUEL_QA_VER
+    FUEL_QA_VER='master'
 fi
-v_env_dir='/qa_environments/fuel-devops-venv/'
 
-echo "Fuel Dev version: ${fuel_devops_ver}"
-echo "Fuel QA branch:   ${fuel_qa_ver}"
+# Erase all previous environments by default
+if [ -z "$ERASE_PREV_ENV" ]; then
+    ERASE_PREV_ENV=true
+fi
+
+V_ENV_DIR="`pwd`/fuel-devops-venv"
+
+echo "Fuel Dev version: ${FUEL_DEV_VER}"
+echo "Fuel QA branch:   ${FUEL_QA_VER}"
 echo ""
 
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install -y python-dev libxml2-dev libxslt1-dev
-sudo apt-get install -y expect
-
-sudo apt-get install -y git \
-postgresql \
-postgresql-server-dev-all \
-libyaml-dev \
-libffi-dev \
-python-dev \
-python-libvirt \
-python-pip \
-qemu-kvm \
-qemu-utils \
-libvirt-bin \
-libvirt-dev \
-ubuntu-vm-builder \
-bridge-utils
-
-sudo apt-get install -y python-virtualenv libpq-dev libgmp-dev
 
 # Check if folder for virtual env exist
-if [ -d "${v_env_dir}" ]; then
-  sudo \rm -rf ${v_env_dir}
+if [ ! -d "${V_ENV_DIR}" ]; then
+    virtualenv --no-site-packages ${V_ENV_DIR}
 fi
-sudo mkdir -p ${v_env_dir}
-sudo chmod 777 ${v_env_dir}
-virtualenv --system-site-packages ${v_env_dir}
 
-source ${v_env_dir}bin/activate
-sudo pip install git+https://github.com/openstack/fuel-devops.git@${fuel_devops_ver} --upgrade
-
-
-sudo virsh pool-define-as --type=dir --name=default --target=/var/lib/libvirt/images
-sudo virsh pool-autostart default
-sudo virsh pool-start default
-
-sudo usermod $(whoami) -a -G libvirtd,sudo
-
-sudo sed -ir 's/peer/trust/' /etc/postgresql/9.*/main/pg_hba.conf
-sudo service postgresql restart
-
-sudo expect -c 'spawn sudo -u postgres createuser -P fuel_devops; expect "password" {send -- "fuel_devops\rfuel_devops\r"};'
-sudo -u postgres createdb fuel_devops -O fuel_devops
+source ${V_ENV_DIR}/bin/activate
+pip install git+https://github.com/openstack/fuel-devops.git@${FUEL_DEV_VER} --upgrade
 
 django-admin.py syncdb --settings=devops.settings
 django-admin.py migrate devops --settings=devops.settings
 
 # erase previous environments
-for i in `dos.py list | grep MOS`; do dos.py erase $i; done
+if [ ${ERASE_PREV_ENV} == true ]; then
+    for i in `dos.py list | grep MOS`; do dos.py erase $i; done
+fi
 
 export ENV_NAME="Test_Deployment_MOS_CI_$RANDOM"
-rm -rf fuel-qa
 
-git clone -b "${fuel_qa_ver}" https://github.com/openstack/fuel-qa
+# Check if fuel-qa folder exist
+if [ ! -d fuel-qa ]; then
+    git clone -b "${FUEL_QA_VER}" https://github.com/openstack/fuel-qa
+else
+    pushd fuel-qa
+    git clean -f -d -x
+    git checkout "${FUEL_QA_VER}"
+    git reset --hard
+    git pull
+    popd
+fi
+
 cp __init__.py fuel-qa/system_test/
 cp deploy_env.py fuel-qa/system_test/tests/
 cp mos_tests.yaml fuel-qa/system_test/tests_templates/devops_configs/
 cp 3_controllers_2compute_neutron*.yaml fuel-qa/system_test/tests_templates/tests_configs
 
 cd fuel-qa
-sudo pip install -r fuelweb_test/requirements.txt
-
-pip install git+https://github.com/openstack/fuel-devops.git@${fuel_devops_ver} --upgrade
+pip install -r fuelweb_test/requirements.txt --upgrade
+# https://bugs.launchpad.net/oslo.service/+bug/1525992 workaround
+pip uninstall -y python-neutronclient
+pip install 'python-neutronclient<4.0.0'
 
 # create new environment
 if [ ${NEUTRONCONF} == "VLAN" ]
 then
     if [ ${CEPH_ENABLE} == "TRUE" ]
     then
-        ./utils/jenkins/system_tests.sh -k -K -j fuelweb_test -t test -v /qa_environments/fuel-devops-venv -w $(pwd) -o --group="system_test.deploy_env(3_controllers_2compute_neutronVLAN_and_ceph_env)"
+        ./utils/jenkins/system_tests.sh -k -K -j fuelweb_test -t test -V ${V_ENV_DIR} -w $(pwd) -o --group="system_test.deploy_env(3_controllers_2compute_neutronVLAN_and_ceph_env)"
     else
-        ./utils/jenkins/system_tests.sh -k -K -j fuelweb_test -t test -v /qa_environments/fuel-devops-venv -w $(pwd) -o --group="system_test.deploy_env(3_controllers_2compute_neutronVLAN_env)"
+        ./utils/jenkins/system_tests.sh -k -K -j fuelweb_test -t test -V ${V_ENV_DIR} -w $(pwd) -o --group="system_test.deploy_env(3_controllers_2compute_neutronVLAN_env)"
     fi
 fi
 
@@ -117,8 +101,8 @@ if [ ${NEUTRONCONF} == "VxLAN" ]
 then
     if [ ${CEPH_ENABLE} == "TRUE" ]
     then
-        ./utils/jenkins/system_tests.sh -k -K -j fuelweb_test -t test -v /qa_environments/fuel-devops-venv -w $(pwd) -o --group="system_test.deploy_env(3_controllers_2compute_neutronVxLAN_and_ceph_env)"
+        ./utils/jenkins/system_tests.sh -k -K -j fuelweb_test -t test -V ${V_ENV_DIR} -w $(pwd) -o --group="system_test.deploy_env(3_controllers_2compute_neutronVxLAN_and_ceph_env)"
     else
-        ./utils/jenkins/system_tests.sh -k -K -j fuelweb_test -t test -v /qa_environments/fuel-devops-venv -w $(pwd) -o --group="system_test.deploy_env(3_controllers_2compute_neutronVxLAN_env)"
+        ./utils/jenkins/system_tests.sh -k -K -j fuelweb_test -t test -V ${V_ENV_DIR} -w $(pwd) -o --group="system_test.deploy_env(3_controllers_2compute_neutronVxLAN_env)"
     fi
 fi
