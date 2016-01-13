adf7659f26f851e4ffa171a183e752fad4d59437
Alexander Gromov <agromov@mirantis.com>
2016-01-13 19:35:33 +0300

    Refactoring and adding configs for different envs.
    1) 4 configs for envs were replaced with 1 template file. Now deploy.sh
    script will construct required config from template file using defined env
    variables.
    
    2) 'system_test.create_deploy_ostf' group was replaced with fuel-qa group
    'create_deploy_ostf'. As result there is no need in __init__.py and
    deploy_env.py files now.
    
    3) Added file openrc which contains all required env variables and
    where we should set necessary values for variables and
    use '. openrc' command before running tests.

--- deploy_env.sh
+++ deploy_env.sh
@@ -2,6 +2,19 @@
 # This script allows to deploy OpenStack environments
 # using simple configuration file
 
+boolean(){
+    if [ -z "$2" ] || [ ${2^^} == 'FALSE' ]
+    then
+        echo 'false'
+    elif [ ${2^^} == 'TRUE' ]
+    then
+        echo 'true'
+    else
+        echo "Please set env variable $1 to empty, 'TRUE' or 'FALSE'."
+        exit 1
+    fi
+}
+
 # Hide trace on jenkins
 if [ -z "$JOB_NAME" ]; then
     set -o xtrace
@@ -13,10 +26,62 @@ then
     exit 1
 fi
 
-if [ -z "$NEUTRONCONF" ] || [ "$NEUTRONCONF" != 'VLAN' -a  "$NEUTRONCONF" != 'VxLAN' ]
+# Replace parameters in config file
+GROUP_NAME='3_controllers_2compute_neutron_env'
+CONFIG_NAME="${GROUP_NAME}.yaml"
+cp 3_controllers_2compute_neutron_env_template.yaml ${CONFIG_NAME}
+
+if [ "$SEGMENT_TYPE" == 'VLAN' ]
 then
-    echo "Please define env variable NEUTRONCONF as 'VLAN' or 'VxLAN'"
+    SEGMENT_TYPE="vlan"
+elif [ "$SEGMENT_TYPE" == 'VxLAN' ]
+then
+    SEGMENT_TYPE="tun"
+else
+    echo "Please define env variable SEGMENT_TYPE as 'VLAN' or 'VxLAN'"
     exit 1
+fi
+
+L2_POP_ENABLE=$(boolean "L2_POP_ENABLE" "$L2_POP_ENABLE")
+DVR_ENABLE=$(boolean "DVR_ENABLE" "$DVR_ENABLE")
+L3_HA_ENABLE=$(boolean "L3_HA_ENABLE" "$L3_HA_ENABLE")
+
+CEPH_ENABLE=$(boolean "CEPH_ENABLE" "$CEPH_ENABLE")
+
+if [ ${L2_POP_ENABLE} == 'true' ]
+then
+    if [ ${SEGMENT_TYPE} != 'tun' ]
+    then
+        echo "Error: L2_POP_ENABLE can be set to TRUE only for VxLAN configuration."
+        exit 1
+    fi
+
+fi
+
+# check limitations
+if [ ${DVR_ENABLE} == 'true' ]
+then
+    if [ ${L3_HA_ENABLE} == 'true' ]
+    then
+        echo "Error: variables DVR_ENABLE and L3_HA_ENABLE can't be TRUE simultaniously."
+        exit 1
+    fi
+    if [ ${SEGMENT_TYPE} == 'tun' ] && [ ${L2_POP_ENABLE} != 'true' ]
+    then
+        echo "Please set env variable L2_POP_ENABLE to 'TRUE' if you want to use VxLAN DVR configuration."
+        exit 1
+    fi
+fi
+
+# replace vars with its values in config files
+sed -i -e "s/<%SEGMENT_TYPE%>/${SEGMENT_TYPE}/g" ${CONFIG_NAME}
+sed -i -e "s/<%L2_POP_ENABLE%>/${L2_POP_ENABLE}/g" ${CONFIG_NAME}
+sed -i -e "s/<%DVR_ENABLE%>/${DVR_ENABLE}/g" ${CONFIG_NAME}
+sed -i -e "s/<%L3_HA_ENABLE%>/${L3_HA_ENABLE}/g" ${CONFIG_NAME}
+
+if [ ${CEPH_ENABLE} == 'true' ]
+then
+    sed -i -e "s/# - ceph-osd/- ceph-osd/" ${CONFIG_NAME}
 fi
 
 # Set fuel dev version
@@ -43,7 +108,6 @@ V_ENV_DIR="`pwd`/fuel-devops-venv"
 echo "Fuel Dev version: ${FUEL_DEV_VER}"
 echo "Fuel QA branch:   ${FUEL_QA_VER}"
 echo ""
-
 
 # Check if folder for virtual env exist
 if [ ! -d "${V_ENV_DIR}" ]; then
@@ -75,10 +139,16 @@ else
     popd
 fi
 
-cp __init__.py fuel-qa/system_test/
-cp deploy_env.py fuel-qa/system_test/tests/
+# TEMPORARY: Delete code below after
+# https://bugs.launchpad.net/fuel/+bug/1529230 merging
+cd fuel-qa
+git fetch https://review.openstack.org/openstack/fuel-qa refs/changes/71/262771/4
+git checkout FETCH_HEAD
+cd ..
+#####
+
 cp mos_tests.yaml fuel-qa/system_test/tests_templates/devops_configs/
-cp 3_controllers_2compute_neutron*.yaml fuel-qa/system_test/tests_templates/tests_configs
+cp ${CONFIG_NAME} fuel-qa/system_test/tests_templates/tests_configs
 
 cd fuel-qa
 pip install -r fuelweb_test/requirements.txt --upgrade
@@ -87,22 +157,4 @@ pip uninstall -y python-neutronclient
 pip install 'python-neutronclient<4.0.0'
 
 # create new environment
-if [ ${NEUTRONCONF} == "VLAN" ]
-then
-    if [ ${CEPH_ENABLE} == "TRUE" ]
-    then
-        ./utils/jenkins/system_tests.sh -k -K -j fuelweb_test -t test -V ${V_ENV_DIR} -w $(pwd) -o --group="system_test.deploy_env(3_controllers_2compute_neutronVLAN_and_ceph_env)"
-    else
-        ./utils/jenkins/system_tests.sh -k -K -j fuelweb_test -t test -V ${V_ENV_DIR} -w $(pwd) -o --group="system_test.deploy_env(3_controllers_2compute_neutronVLAN_env)"
-    fi
-fi
-
-if [ ${NEUTRONCONF} == "VxLAN" ]
-then
-    if [ ${CEPH_ENABLE} == "TRUE" ]
-    then
-        ./utils/jenkins/system_tests.sh -k -K -j fuelweb_test -t test -V ${V_ENV_DIR} -w $(pwd) -o --group="system_test.deploy_env(3_controllers_2compute_neutronVxLAN_and_ceph_env)"
-    else
-        ./utils/jenkins/system_tests.sh -k -K -j fuelweb_test -t test -V ${V_ENV_DIR} -w $(pwd) -o --group="system_test.deploy_env(3_controllers_2compute_neutronVxLAN_env)"
-    fi
-fi
+./utils/jenkins/system_tests.sh -k -K -j fuelweb_test -t test -v ${V_ENV_DIR} -w $(pwd) -o --group="system_test.create_deploy_ostf($GROUP_NAME)"
