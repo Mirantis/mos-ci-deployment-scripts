4c3c424e821b2f8db44e267fd982ba3f5b86fd32
Eduard. Fazliev <efazliev@srv103-bud.infra.mirantis.net>
2016-02-09 18:09:31 +0300

    Add ansible roles to prepare jenkins slaves.

---
+++ ansible-playbooks/roles/test_rail/files/user.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+TESTRAIL_URL=https://mirantis.testrail.com
+TESTRAIL_USER=releaseacceptance@mirantis.com
+TESTRAIL_PASSWORD=Release6.1
+TESTRAIL_PROJECT='Mirantis OpenStack'
+TESTRAIL_MILESTONE=8.0
+TESTRAIL_SUITE='[8.0][MOSQA] Automated Cloud Testing'
