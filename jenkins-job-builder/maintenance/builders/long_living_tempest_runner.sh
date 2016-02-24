51d52db4fcc6d8c47210a2db95924a5d4faf517e
vrovachev <vrovachev@mirantis.com>
2016-02-25 01:50:21 +0300

    Hotfix for longliving tests
    Change script name in builders file

--- jenkins-job-builder/maintenance/builders/long_living_tempest_runner.sh
+++ jenkins-job-builder/maintenance/builders/long_living_tempest_runner.sh
@@ -6,5 +6,5 @@ wget https://raw.githubusercontent.com/Mirantis/mos-ci-deployment-scripts/master
 
 chmod +x long_living_rally_run.sh
 
-sshpass -p 'r00tme' scp -o "StrictHostKeyChecking no" run_rally.sh root@10.20.1.2:/root/
+sshpass -p 'r00tme' scp -o "StrictHostKeyChecking no" long_living_rally_run.sh root@10.20.1.2:/root/
 echo 'chmod +x /root/long_living_rally_run.sh && /bin/bash -xe /root/long_living_rally_run.sh > /root/log.log' | sshpass -p 'r00tme' ssh -T root@10.20.1.2
