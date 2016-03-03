6dd9221586405f569e788d4b9476ef23f66538c3
efazliev <efazliev@mera.ru>
2016-03-03 17:08:41 +0300

    fixed PARSED_LINK for mitaka jobs

--- jenkins-job-builder/product-9.0/mitaka/superjobs/neutron/9.0_mitaka_iso_and_erase_env.sh
+++ jenkins-job-builder/product-9.0/mitaka/superjobs/neutron/9.0_mitaka_iso_and_erase_env.sh
@@ -1,6 +1,6 @@
 sudo rm -rf mos-ci-deployment-scripts
 git clone https://github.com/Mirantis/mos-ci-deployment-scripts.git
-PARSED_LINK='https://product-ci.infra.mirantis.net/view/9.0-mitaka/job/9.0-mitaka.all/lastSuccessfulBuild/'
+PARSED_LINK='https://product-ci.infra.mirantis.net/view/9.0-mitaka/job/9.0-mitaka.all/lastSuccessfulBuild/api/python'
 ISO_DIR='/var/www/fuelweb-iso'
 cd mos-ci-deployment-scripts/jenkins-job-builder/python_scripts/9.0_parse_jenkins_for_iso
 sudo python parser.py --link "$PARSED_LINK" -d "$ISO_DIR"
