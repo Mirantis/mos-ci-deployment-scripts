f248623cd8dc83a651ac890a987bb14a89dfb219
efazliev <efazliev@mera.ru>
2016-03-14 18:54:45 +0300

    added +e to script

--- jenkins-job-builder/product-9.0/mitaka/superjobs/neutron/9.0_mitaka_iso_and_erase_env.sh
+++ jenkins-job-builder/product-9.0/mitaka/superjobs/neutron/9.0_mitaka_iso_and_erase_env.sh
@@ -1,3 +1,4 @@
+
 sudo rm -rf mos-ci-deployment-scripts
 git clone https://github.com/Mirantis/mos-ci-deployment-scripts.git
 PARSED_LINK='https://product-ci.infra.mirantis.net/job/9.0.all/lastSuccessfulBuild/api/python'
@@ -6,6 +7,7 @@ cd mos-ci-deployment-scripts/jenkins-job-builder/python_scripts/9.0_parse_jenkin
 
 iso_name=`sudo python parser.py --link "$PARSED_LINK" -d "$ISO_DIR" --link-only`
 
+set +e
 env_presents=`sudo dos.py list | grep "$iso_name"`
 
 if [[ "$env_presents" != '' ]];
@@ -19,7 +21,6 @@ sudo python parser.py --link "$PARSED_LINK" -d "$ISO_DIR"
 # sudo rm -rf /var/www/fuelweb-iso
 # sudo wget http://srv65-bud.infra.mirantis.net/fuelweb-iso/fuel-9.0-mitaka-48-2016-02-29_06-16-00.iso -P /var/www/fuelweb-iso
 
-set +e
 sudo dos.py list > temp
 while read -r line
 do
