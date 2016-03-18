e36fc5e6deebbce2f6f0e4f247c67d4c18d9664a
vrovachev <vrovachev@mirantis.com>
2016-03-18 10:34:59 +0300

    Remove exit 1 for non-fully successed tempest run for maintenance jobs

---
+++ jenkins-job-builder/maintenance/helpers/testrail.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -ex
+
+CUSTOM_VERSION=${CUSTOM_VERSION:-MU-rc}
+echo "$CUSTOM_VERSION"_"$TEST_GROUP" > build-name-setter.info
+
+if [ ! -f $REPORT_XML ]; then
+    echo "Can't find $REPORT_XML file"
+    exit 1
+fi
+
+virtualenv venv
+source venv/bin/activate
+source "$TESTRAIL_FILE"
+
+if [ -n "$SUITE" ]; then
+    export TESTRAIL_TEST_SUITE="$SUITE"
+fi
+
+if [ -n "$MILESTONE" ]; then
+    export TESTRAIL_MILESTONE="$MILESTONE"
+fi
+
+if [ -d fuel-qa ]; then
+    rm -rf fuel-qa
+fi
+
+git clone https://github.com/openstack/fuel-qa.git
+pip install launchpadlib
+pip install simplejson
+cd fuel-qa
+export PYTHONPATH="$(pwd):$PYTHONPATH"
+python fuelweb_test/testrail/report_tempest_results.py -r "$TEST_GROUP" -i "${CUSTOM_VERSION}" -p "${REPORT_XML}"
