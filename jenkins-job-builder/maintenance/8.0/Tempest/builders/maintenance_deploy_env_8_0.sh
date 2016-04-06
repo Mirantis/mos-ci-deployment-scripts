8562404c897129e866a19a8a16002b8cbed75328
vrovachev <vrovachev@mirantis.com>
2016-04-06 19:14:36 +0300

    Replaced 8.0 tempest jobs.

---
+++ jenkins-job-builder/maintenance/8.0/Tempest/builders/maintenance_deploy_env_8_0.sh
@@ -0,0 +1,194 @@
+#!/bin/bash
+# This script allows to deploy OpenStack environments
+# using simple configuration file
+
+# Hide trace on jenkins
+if [ -z "$JOB_NAME" ]; then
+    set -o xtrace
+fi
+
+SKIP_INSTALL_ENV=${SKIP_INSTALL_ENV:-false}
+
+if $SKIP_INSTALL_ENV ; then
+    exit 0
+fi
+
+# exit from shell if error happens
+set -ex
+
+# Download and link ISO
+ISO_PATH=$(seedclient-wrapper -d -m "${MAGNET_LINK}" -v --force-set-symlink -o "${WORKSPACE}")
+
+# Source python virtualenv and run db migration
+source ${VENV_PATH}/bin/activate
+pip install -U pip
+
+django-admin.py syncdb --settings=devops.settings
+django-admin.py migrate devops --settings=devops.settings
+
+if [ -z "$ISO_PATH" ]
+then
+    echo "Please download ISO and define env variable ISO_PATH"
+    exit 1
+fi
+
+# Bonding
+BONDING=${BONDING:-false}
+
+# Set env name
+ENV_NAME=${ENV_NAME:-maintenance_env_8.0}
+
+# Set fuel QA version
+# https://github.com/openstack/fuel-qa/branches
+FUEL_QA_VER=${FUEL_QA_VER:-stable/8.0}
+
+# Erase all previous environments by default
+ERASE_PREV_ENV=${ERASE_PREV_ENV:-true}
+
+# Set GROUP. By default tempest_ceph_services
+GROUP=${GROUP:-tempest_ceph_services}
+DISABLE_SSL=${DISABLE_SSL:-false}
+
+OPENSTACK_RELEASE=${OPENSTACK_RELEASE:-ubuntu}
+
+# Check if fuel-qa folder exist
+if [ ! -d fuel-qa ]; then
+    git clone -b "${FUEL_QA_VER}" https://github.com/openstack/fuel-qa
+else
+    pushd fuel-qa
+    git clean -f -d -x
+    git checkout "${FUEL_QA_VER}"
+    git reset --hard
+    git pull
+    popd
+fi
+
+# erase previous environments
+if ${ERASE_PREV_ENV} ; then
+    dos.py list | tail -n+3 | xargs -I {} dos.py erase {}
+fi
+
+if [ -n "${FILE}" ]; then
+    cat jenkins-job-builder/maintenance/helpers/${FILE} > fuel-qa/fuelweb_test/tests/test_services.py
+fi
+
+cd fuel-qa
+
+# Apply fuel-qa patches
+if [[ ${IRONIC_ENABLE} == 'true' ]]; then
+    file_name=ironic.patch
+    patch_file=../fuel_qa_patches/$file_name
+    echo "Check for patch $file_name"
+    git apply --check $patch_file 2> /dev/null
+    if [ $? -eq 0 ]; then
+        echo "Applying patch $file_name"
+        git apply $patch_file
+    fi
+fi
+
+if [[ ${DVR_ENABLE} == 'true' ]] || [[ ${L3_HA_ENABLE} == 'true' ]] || [[ ${L2_POP_ENABLE} == 'true' ]]; then
+    file_name=DVR_L2_pop_HA.patch
+    patch_file=../fuel_qa_patches/$file_name
+    echo "Check for patch $file_name"
+    git apply --check $patch_file 2> /dev/null
+    if [ $? -eq 0 ]; then
+        echo "Applying patch $file_name"
+        git apply $patch_file
+    fi
+fi
+
+###################### Get MIRROR_UBUNTU ###############
+
+MIRROR_HOST="http://mirror.seed-cz1.fuel-infra.org/"
+
+if [[ ! "${MIRROR_UBUNTU}" ]]; then
+
+    case "${UBUNTU_MIRROR_ID}" in
+        latest)
+            UBUNTU_MIRROR_URL="$(curl ${MIRROR_HOST}pkgs/ubuntu-latest.htm)"
+            ;;
+        *)
+            UBUNTU_MIRROR_URL="${MIRROR_HOST}pkgs/${UBUNTU_MIRROR_ID}/"
+    esac
+
+    UBUNTU_REPOS="deb ${UBUNTU_MIRROR_URL} trusty main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-updates main universe multiverse|deb ${UBUNTU_MIRROR_URL} trusty-security main universe multiverse"
+
+    if [ "$ENABLE_PROPOSED" = true ]; then
+        UBUNTU_PROPOSED="deb ${UBUNTU_MIRROR_URL} trusty-proposed main universe multiverse"
+        UBUNTU_REPOS="$UBUNTU_REPOS|$UBUNTU_PROPOSED"
+    fi
+
+    export MIRROR_UBUNTU="$UBUNTU_REPOS"
+
+fi
+
+###################### Set extra DEB and RPM repos ####
+
+if [[ -n "${RPM_LATEST}" ]]; then
+    RPM_MIRROR="${MIRROR_HOST}mos-repos/centos/mos8.0-centos7-fuel/snapshots/"
+    if [[ "${ENABLE_PROPOSED}" == "true" ]]; then
+        RPM_PROPOSED="mos-proposed,${RPM_MIRROR}proposed-${RPM_LATEST}/x86_64"
+        EXTRA_RPM_REPOS+="${RPM_PROPOSED}"
+        UPDATE_FUEL_MIRROR="${RPM_MIRROR}proposed-${RPM_LATEST}/x86_64"
+    fi
+    if [[ "${ENABLE_UPDATES}" == "true" ]]; then
+        RPM_UPDATES="mos-updates,${RPM_MIRROR}updates-${RPM_LATEST}/x86_64"
+        if [[ -n "${EXTRA_RPM_REPOS}" ]]; then
+            EXTRA_RPM_REPOS+="|"
+            UPDATE_FUEL_MIRROR+=" "
+        fi
+        EXTRA_RPM_REPOS+="${RPM_UPDATES}"
+        UPDATE_FUEL_MIRROR+="${RPM_MIRROR}updates-${RPM_LATEST}/x86_64"
+    fi
+    if [[ "${ENABLE_SECURITY}" == "true" ]]; then
+        RPM_SECURITY="mos-security,${RPM_MIRROR}security-${RPM_LATEST}/x86_64"
+        if [[ -n "${EXTRA_RPM_REPOS}" ]]; then
+            EXTRA_RPM_REPOS+="|"
+            UPDATE_FUEL_MIRROR+=" "
+        fi
+        EXTRA_RPM_REPOS+="${RPM_SECURITY}"
+        UPDATE_FUEL_MIRROR+="${RPM_MIRROR}security-${RPM_LATEST}/x86_64"
+    fi
+    export EXTRA_RPM_REPOS
+    export UPDATE_FUEL_MIRROR
+    export UPDATE_MASTER=true
+fi
+
+if [[ -n "${DEB_LATEST}" ]]; then
+    DEB_MIRROR="${MIRROR_HOST}mos-repos/ubuntu/snapshots/8.0-${DEB_LATEST}"
+    if [[ "${ENABLE_PROPOSED}" == "true" ]]; then
+        DEB_PROPOSED="mos-proposed,deb ${DEB_MIRROR} mos8.0-proposed main restricted"
+        EXTRA_DEB_REPOS+="${DEB_PROPOSED}"
+    fi
+    if [[ "${ENABLE_UPDATES}" == "true" ]]; then
+        DEB_UPDATES="mos-updates,deb ${DEB_MIRROR} mos8.0-updates main restricted"
+        if [[ -n "${EXTRA_DEB_REPOS}" ]]; then
+            EXTRA_DEB_REPOS+="|"
+        fi
+        EXTRA_DEB_REPOS+="${DEB_UPDATES}"
+    fi
+    if [[ "${ENABLE_SECURITY}" == "true" ]]; then
+        DEB_SECURITY="mos-security,deb ${DEB_MIRROR} mos8.0-security main restricted"
+        if [[ -n "${EXTRA_DEB_REPOS}" ]]; then
+            EXTRA_DEB_REPOS+="|"
+        fi
+        EXTRA_DEB_REPOS+="${DEB_SECURITY}"
+    fi
+    export EXTRA_DEB_REPOS
+fi
+
+
+# create new environment
+# more time can be required to deploy env
+export DEPLOYMENT_TIMEOUT=10000
+export ENV_NAME=$ENV_NAME
+export ADMIN_NODE_MEMORY=4096
+export SLAVE_NODE_CPU=3
+export SLAVE_NODE_MEMORY=16384
+export DISABLE_SSL=$DISABLE_SSL
+export NOVA_QUOTAS_ENABLED=true
+export KVM_USE=true
+export BONDING=$BONDING
+export OPENSTACK_RELEASE=$OPENSTACK_RELEASE
+
+./utils/jenkins/system_tests.sh -k -K -j fuelweb_test -t test -w $(pwd) -e "$ENV_NAME" -o --group="$GROUP" -i "$ISO_PATH"
--- jenkins-job-builder/maintenance/helpers/rally_run.sh
+++ jenkins-job-builder/maintenance/helpers/rally_run.sh
@@ -53,7 +53,12 @@ ID=$(docker images | awk '/rally/{print $3}')
 echo "ID: ${ID}"
 DOCK_ID=$(docker run -tid -v /var/lib/rally-tempest-container-home-dir:/home/rally --net host "$ID")
 echo "DOCK ID: ${DOCK_ID}"
+sed -i "s|:5000|:5000/v2.0|g" /var/lib/rally-tempest-container-home-dir/openrc
+docker exec -u root "$DOCK_ID" sed -i "s|\#swift_operator_role = Member|swift_operator_role=SwiftOperator|g" /etc/rally/rally.conf
 docker exec "$DOCK_ID" setup-tempest
+file=`find / -name tempest.conf`
+echo "backup = False" >> $file
+sed -i "95i ironic = False" $file
 docker exec "$DOCK_ID" bash -c "source /home/rally/openrc && rally verify start --system-wide"
 docker exec "$DOCK_ID" bash -c "rally verify results --json --output-file output.json"
 docker exec "$DOCK_ID" bash -c "rm -rf rally_json2junit && git clone https://github.com/EduardFazliev/rally_json2junit.git && python rally_json2junit/rally_json2junit/results_parser.py output.json"
