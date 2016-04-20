2d2cb46b21f6ea8bcd7a99e4a593d8a7504b196d
vrovachev <vrovachev@mirantis.com>
2016-04-20 11:26:02 +0300

    Move tempest 8.0 to new testrail reporter

--- jenkins-job-builder/maintenance/helpers/testrail.sh
+++ jenkins-job-builder/maintenance/helpers/testrail.sh
@@ -1,36 +1,35 @@
 #!/bin/bash -ex
 
-CUSTOM_VERSION=${CUSTOM_VERSION:-MU-rc}
-config_name=${CONFIG_NAME:-Ubuntu 14.04}
-echo $config_name
+SNAPSHOT=$(echo $SNAPSHOT_NAME | sed 's/ha_deploy_//')
 
-echo "$CUSTOM_VERSION"_"$TEST_GROUP" > build-name-setter.info
+echo 8.0_"$ENV_NAME"__"$SNAPSHOT" > build-name-setter.info
+
+REPORT_XML="${REPORT_PREFIX}/${ENV_NAME}_${SNAPSHOT_NAME}/{$REPORT_FILE}"
 
 if [ ! -f $REPORT_XML ]; then
     echo "Can't find $REPORT_XML file"
     exit 1
 fi
 
+# standard credentials for testrail is in this file, that
+# is copied to host by ansible playbook
+source "${TESTRAIL_FILE}"
+
+# if we need to change SUITE
+if [ -n "$SUITE" ]; then
+    TESTRAIL_SUITE="${SUITE}"
+fi
+
+# if we need to change MILESTONE
+if [ -n "${MILESTONE}" ]; then
+    TESTRAIL_MILESTONE="${MILESTONE}"
+fi
+
+if ${ADD_TIMESTAMP}; then
+    TESTRAIL_PLAN_NAME+=" #$(date +%Y/%m/%d)"
+fi
+
 virtualenv venv
 source venv/bin/activate
-source "$TESTRAIL_FILE"
-
-if [ -n "$SUITE" ]; then
-    export TESTRAIL_TEST_SUITE=${SUITE}
-fi
-
-if [ -n "$MILESTONE" ]; then
-    export TESTRAIL_MILESTONE=${MILESTONE}
-fi
-
-if [ -d fuel-qa ]; then
-    rm -rf fuel-qa
-fi
-
-git clone https://github.com/openstack/fuel-qa.git
-cd fuel-qa
-pip install simplejson
-pip install -r fuelweb_test/requirements.txt --upgrade
-export PYTHONPATH="$(pwd):$PYTHONPATH"
-export USE_CENTOS=$USE_CENTOS
-python fuelweb_test/testrail/report_tempest_results.py -r "${TEST_GROUP}" -c "${config_name}" -i ${CUSTOM_VERSION} -p ${REPORT_XML}
+python setup.py install
+report -v --testrail-plan-name "$TESTRAIL_PLAN_NAME" --env-description "$SNAPSHOT-$TEST_GROUP" --testrail-user  "${TESTRAIL_USER}" --testrail-password "${TESTRAIL_PASSWORD}" --testrail-project "${TESTRAIL_PROJECT}" --testrail-milestone "${TESTRAIL_MILESTONE}" --testrail-suite "${TESTRAIL_SUITE}" --test-results-link "$BUILD" "$REPORT_XML"
