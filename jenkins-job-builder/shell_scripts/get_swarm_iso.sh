465e188e033733f3567a7d390721f4c0403c55cf
Alexander Gubanov <greatehop@gmail.com>
2016-06-14 16:10:43 +0300

    Temporarily set custom ISO for SWARM tests
    Change-Id: Iac3e8d86d96444e258b43e6b994e8f457cc7e114

--- jenkins-job-builder/shell_scripts/get_swarm_iso.sh
+++ jenkins-job-builder/shell_scripts/get_swarm_iso.sh
@@ -11,7 +11,8 @@ sudo rm -rf init_env.py
 sudo wget https://raw.githubusercontent.com/Mirantis/mos-ci-deployment-scripts/master/jenkins-job-builder/python_scripts/9.0_init_env_for_job/init_env.py
 sudo chmod +x init_env.py
 
-SWARM_ISO_LINK=`python init_env.py`
+#SWARM_ISO_LINK=`python init_env.py`
+SWARM_ISO_LINK="http://srv52-bud.infra.mirantis.net/fuelweb-iso/fuel-9.0-mos-465-2016-06-09_22-51-38.iso"
 
 sudo rm -rf "$ISO_DIR"/*
 sudo wget "$SWARM_ISO_LINK" -P "$ISO_DIR"
