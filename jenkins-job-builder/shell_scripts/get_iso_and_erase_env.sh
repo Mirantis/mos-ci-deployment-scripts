569942fdb2c4f2c529bef5274169bc8cfc3f932c
Alexander Gubanov <greatehop@gmail.com>
2016-06-16 16:46:18 +0300

    Temporarily set custom ISO for tempest tests
    It needs for getting results on ISO 490 during night run tempest.
    Should be reverted after tests will be started.
    
    Change-Id: I62eefe55a20a9930eca32b8ecc3601bbc99b235b

--- jenkins-job-builder/shell_scripts/get_iso_and_erase_env.sh
+++ jenkins-job-builder/shell_scripts/get_iso_and_erase_env.sh
@@ -8,7 +8,10 @@ sudo rm -rf init_env.py
 sudo wget https://raw.githubusercontent.com/Mirantis/mos-ci-deployment-scripts/master/jenkins-job-builder/python_scripts/9.0_init_env_for_job/init_env.py
 sudo chmod +x init_env.py
 
-iso_link=`python init_env.py`
+#iso_link=`python init_env.py`
+
+# uncomment and set link for special ISO
+iso_link="http://srv52-bud.infra.mirantis.net/fuelweb-iso/fuel-9.0-mos-490-2016-06-15_21-03-09.iso"
 
 ######workaround for product-ci bug#################
 #iso_link=`python init_env.py | sed 's/iso"/iso/g'`#
@@ -16,11 +19,6 @@ iso_link=`python init_env.py`
 
 sudo rm -rf /var/www/fuelweb-iso/*
 sudo wget "$iso_link" -P /var/www/fuelweb-iso/
-
-#####if we need some special iso#####
-#sudo rm -rf /var/www/fuelweb-iso   #
-#sudo wget  -P /var/www/fuelweb-iso #
-#####################################
 
 sudo dos.py list > temp
 while read -r line
