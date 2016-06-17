1e9435d1e949fbbeca367b37968d0ef903c82067
Alexander Gromov <agromov@mirantis.com>
2016-06-17 18:11:26 +0300

    Removed workaround for the closed bug
    As the bug #1525992 has been closed we don't need workaround for it
    anymore.
    
    Change-Id: Ie7133fae0a40704347922ee2d190fc4ae4a82441

--- deploy_env.sh
+++ deploy_env.sh
@@ -317,9 +317,6 @@ else
 fi
 
 pip install -r fuel-qa/fuelweb_test/requirements.txt --upgrade
-# https://bugs.launchpad.net/oslo.service/+bug/1525992 workaround
-pip uninstall -y python-neutronclient
-pip install 'python-neutronclient<4.0.0'
 
 django-admin.py syncdb --settings=devops.settings
 django-admin.py migrate devops --settings=devops.settings
