173956385d12aa8886a7816c6f363922f0a02c5f
Alexander Gubanov <greatehop@gmail.com>
2016-06-17 16:26:04 +0300

    Return back to usual workflow for tempest test
    Revert "Temporarily set custom ISO for tempest tests"
    This reverts commit 569942fdb2c4f2c529bef5274169bc8cfc3f932c
    
    Revert "Shift time for night run of tempest test"
    This reverts commit d8a40394e219d7b192ddfa99351c22621e6f275b
    
    Change-Id: Ic75f55c71fa73055646279a18d2e40d52bf4c8f8

--- jenkins-job-builder/shell_scripts/get_iso_and_erase_env.sh
+++ jenkins-job-builder/shell_scripts/get_iso_and_erase_env.sh
@@ -8,10 +8,10 @@ sudo rm -rf init_env.py
 sudo wget https://raw.githubusercontent.com/Mirantis/mos-ci-deployment-scripts/master/jenkins-job-builder/python_scripts/9.0_init_env_for_job/init_env.py
 sudo chmod +x init_env.py
 
-#iso_link=`python init_env.py`
+iso_link=`python init_env.py`
 
 # uncomment and set link for special ISO
-iso_link="http://srv52-bud.infra.mirantis.net/fuelweb-iso/fuel-9.0-mos-490-2016-06-15_21-03-09.iso"
+#iso_link=""
 
 ######workaround for product-ci bug#################
 #iso_link=`python init_env.py | sed 's/iso"/iso/g'`#
@@ -23,6 +23,6 @@ sudo wget "$iso_link" -P /var/www/fuelweb-iso/
 sudo dos.py list > temp
 while read -r line
 do
-set -e
-sudo dos.py erase $line || true
+    set -e
+    sudo dos.py erase $line || true
 done < temp
