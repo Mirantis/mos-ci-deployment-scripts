0a80a16de0621ffe5e8695a7448ab8d4a53e50dc
Alexander Koryagin <akoryag@mera.ru>
2016-07-04 13:22:56 +0000

    Merge of "get_plugins.sh" from master branch
    Change-Id: Iec118be4425931b5e2ebc1536a599874710317ba

--- jenkins-job-builder/shell_scripts/get_plugins.sh
+++ jenkins-job-builder/shell_scripts/get_plugins.sh
@@ -1,28 +1,9 @@
 #!/usr/bin/env bash
-PLUGINS_DIR="${HOME}/detach-plugins/"
-PARSED_PLUGINS_LINK=${PARSED_PLUGINS_LINK:-"http://jenkins-product.srt.mirantis.net:8080/view/plugins/job/build-fuel-plugins/"}
+PLUGINS_ARCHIVE=${PLUGINS_ARCHIVE:-"http://cz7776.bud.mirantis.net:8080/jenkins/job/build_plugins/lastSuccessfulBuild/artifact/*zip*/archive.zip"}
 
-rm -rf "$PLUGINS_DIR"/*
-git clone https://review.gerrithub.io/Mirantis/mos-ci-deployment-scripts
-python mos-ci-deployment-scripts/jenkins-job-builder/python_scripts/parse_jenkins_reports/parser.py --link "$PARSED_PLUGINS_LINK" -d "$PLUGINS_DIR" --type plugins
-rm -rf mos-ci-deployment-scripts
-
-
-for file in `find $PLUGINS_DIR -type f -name "*.rpm"`
-do
-    if [[ $file =~ .*rabbit.* ]]
-    then
-        SEPARATE_SERVICE_RABBIT_PLUGIN_PATH=$file
-    fi
-
-    if [[ $file =~ .*database.* ]]
-    then
-        SEPARATE_SERVICE_DB_PLUGIN_PATH=$file
-    fi
-
-    if [[ $file =~ .*keystone.* ]]
-    then
-        SEPARATE_SERVICE_KEYSTONE_PLUGIN_PATH=$file
-    fi
-
-done
+TMPFILE=$(mktemp)
+export PLUGINS_PATH=$(pwd)/fuel_plugins
+wget $PLUGINS_ARCHIVE -O $TMPFILE
+unzip -jo $TMPFILE '*.rpm' -d $PLUGINS_PATH
+rm $TMPFILE
+env > "$ENV_INJECT_PATH"
