1d888100ed00c006007785bed0a2348ed224fb4b
Yury Tregubov <ytregubov@mirantis.com>
2016-07-08 17:26:27 +0400

    obtain fresh plugins from jenkis job
    Change-Id: Ie3026635e3732c2f67bd640083c1881c1c43e165

--- jenkins-job-builder/shell_scripts/get_plugins.sh
+++ jenkins-job-builder/shell_scripts/get_plugins.sh
@@ -1,31 +1,9 @@
 #!/usr/bin/env bash
-PLUGINS_DIR=${PLUGINS_DIR:-"/var/www/detach-plugins/"}
-PARSED_PLUGINS_LINK=${PARSED_PLUGINS_LINK:-"http://jenkins-product.srt.mirantis.net:8080/view/plugins/job/build-fuel-plugins/"}
+PLUGINS_ARCHIVE=${PLUGINS_ARCHIVE:-"http://cz7776.bud.mirantis.net:8080/jenkins/job/build_plugins/lastSuccessfulBuild/artifact/*zip*/archive.zip"}
 
-sudo rm -rf "$PLUGINS_DIR"/*
-git clone https://review.gerrithub.io/Mirantis/mos-ci-deployment-scripts
-sudo python mos-ci-deployment-scripts/jenkins-job-builder/python_scripts/parse_jenkins_reports/parser.py --link "$PARSED_PLUGINS_LINK" -d "$PLUGINS_DIR" --type plugins
-rm -rf mos-ci-deployment-scripts
-
-
-for file in `find $PLUGINS_DIR -type f -name "*.rpm"`
-do
-    if [[ $file =~ .*rabbit.* ]]
-    then
-        SEPARATE_SERVICE_RABBIT_PLUGIN_PATH=$file
-        echo "SEPARATE_SERVICE_RABBIT_PLUGIN_PATH=${SEPARATE_SERVICE_RABBIT_PLUGIN_PATH}" >> "$ENV_INJECT_PATH"
-    fi
-
-    if [[ $file =~ .*database.* ]]
-    then
-        SEPARATE_SERVICE_DB_PLUGIN_PATH=$file
-        echo "SEPARATE_SERVICE_DB_PLUGIN_PATH=${SEPARATE_SERVICE_DB_PLUGIN_PATH}" >>  "$ENV_INJECT_PATH"
-    fi
-
-    if [[ $file =~ .*keystone.* ]]
-    then
-        SEPARATE_SERVICE_KEYSTONE_PLUGIN_PATH=$file
-        echo "SEPARATE_SERVICE_KEYSTONE_PLUGIN_PATH=${SEPARATE_SERVICE_KEYSTONE_PLUGIN_PATH}" >>  "$ENV_INJECT_PATH"
-    fi
-
-done
+TMPFILE=$(mktemp)
+export PLUGINS_PATH=$(pwd)/fuel_plugins
+wget $PLUGINS_ARCHIVE -O $TMPFILE
+unzip -jo $TMPFILE '*.rpm' -d $PLUGINS_PATH
+rm $TMPFILE
+env > "$ENV_INJECT_PATH"
