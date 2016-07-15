cc8a6a8956770e0b239b765a062d1fb664797bde
Yury Tregubov <ytregubov@mirantis.com>
2016-07-15 15:42:21 +0400

    moved heat and failover tests to tox based jobs added fule-ldap
    Also updated get_plugins.sh to obtain rabbit pligin from
    https://plugin-ci.fuel-infra.org
    
    Change-Id: Ia24ffaacf5d02f856d958e945c7fef9b17a1cd9c

--- jenkins-job-builder/shell_scripts/get_plugins.sh
+++ jenkins-job-builder/shell_scripts/get_plugins.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
-PLUGINS_ARCHIVE=${PLUGINS_ARCHIVE:-"http://cz7776.bud.mirantis.net:8080/jenkins/job/build_plugins/lastSuccessfulBuild/artifact/*zip*/archive.zip"}
-
-TMPFILE=$(mktemp)
 export PLUGINS_PATH=$(pwd)/fuel_plugins
+
+PLUGINS_ARCHIVE=${PLUGINS_ARCHIVE:-"http://cz7776.bud.mirantis.net:8080/jenkins/job/build_plugins/lastSuccessfulBuild/artifact/*zip*/archive.zip"}
+TMPFILE=$(mktemp)
 wget $PLUGINS_ARCHIVE -O $TMPFILE
 unzip -jo $TMPFILE '*.rpm' -d $PLUGINS_PATH
 rm $TMPFILE
+
+# Get rabbit plugin separately
+TMPFILE=$(mktemp)
+wget https://plugin-ci.fuel-infra.org/job/9.0.fuel-plugin.detach-rabbitmq.build/lastSuccessfulBuild/artifact/*zip*/archive.zip -O $TMPFILE
+unzip -jo $TMPFILE '*.rpm' -d $PLUGINS_PATH
+rm $TMPFILE
+
+export LDAP_CONFIG_FILE=$PWD/ldap_config.yaml 
+wget https://raw.githubusercontent.com/Mirantis/mos-ci-deployment-scripts/stable/9.0/plugins_config/ldap.yaml -O $LDAP_CONFIG_FILE 
+
 env > "$ENV_INJECT_PATH"
