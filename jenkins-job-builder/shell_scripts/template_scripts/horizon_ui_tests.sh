3ae2bf81d098ada48e78ddfc942006f470db1307
Sergei Chipiga <schipiga@mirantis.com>
2016-08-01 17:52:29 +0300

    Add job to run horizon UI autotests with steps architecture.
    Change-Id: I1aeefb3c42f1536e9d7f99b477eda26dcd62ead7

---
+++ jenkins-job-builder/shell_scripts/template_scripts/horizon_ui_tests.sh
@@ -0,0 +1,38 @@
+set +e
+
+dos.py revert-resume "$ENV_NAME" "$SNAPSHOT_NAME"
+
+sudo apt-get update
+#TBD need to remove firefox updaed since after FF 47.0 selenium tests are
+#failed so far @schipiga will investigate on how to made that update
+sudo apt-get -y install xvfb python-virtualenv libav-tools
+
+git clone -b debug https://github.com/sergeychipiga/horizon_autotests.git
+cd horizon_autotests
+
+virtualenv .venv
+. .venv/bin/activate
+
+pip install -U pip
+pip install -r requirements
+pip install -e .
+
+export DASHBOARD_URL='http://10.109.4.6/horizon'
+
+printenv || true
+
+VIRTUAL_DISPLAY=1 py.test horizon_autotests -v --junitxml=report.xml
+
+deactivate
+
+cp "$REPORT_FILE" ../
+cp *.log ../
+
+sudo mkdir -p "$REPORT_PREFIX"/"$ENV_NAME"_"$SNAPSHOT_NAME" && \
+sudo cp "$REPORT_FILE" "$REPORT_PREFIX"/"$ENV_NAME"_"$SNAPSHOT_NAME" && \
+sudo cp *.log "$REPORT_PREFIX"/"$ENV_NAME"_"$SNAPSHOT_NAME" \
+|| true
+
+dos.py destroy "$ENV_NAME"
+
+exit 0
