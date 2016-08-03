bf08597870d7e38d3ca26a0e344c0ff20298dfbb
Yury Tregubov <ytregubov@mirantis.com>
2016-08-03 12:40:09 +0400

    removing unused shell scripts
    Change-Id: I0236cdfa4d46719215eed42896a8ee37f5e04e42

--- jenkins-job-builder/shell_scripts/create_venv.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-virtualenv "$PY_VENV"
-. "$PY_VENV"/bin/activate
-pip install python-jenkins
-deactivate
--- jenkins-job-builder/shell_scripts/erase_all_envs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo dos.py list > temp
-while read -r line
-do
-set -e
-sudo dos.py erase $line || true
-done < temp
