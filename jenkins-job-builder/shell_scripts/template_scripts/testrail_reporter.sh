c5c97c73b96bdc7b680045e1609db62662227ae3
Yury Tregubov <ytregubov@mirantis.com>
2016-08-05 13:59:26 +0400

    removed dummy source of user.sh in the reporter
    Change-Id: Id86c93e079cce844b0e7ac0bfe55aeaa59af3e11

--- jenkins-job-builder/shell_scripts/template_scripts/testrail_reporter.sh
+++ jenkins-job-builder/shell_scripts/template_scripts/testrail_reporter.sh
@@ -8,21 +8,12 @@ virtualenv --clear testrail
 # NEED FIX! (move scripts from custom repo to Mirantis repo)
 pip install git+https://github.com/gdyuldin/testrail_reporter.git@stable
 
-source "$TESTRAIL_FILE"
-
-# if we need to change SUITE
-if [ -n "$SUITE" ];
-then
-    TESTRAIL_SUITE="$SUITE"
-    export TESTRAIL_SUITE="$SUITE"
-fi
-
-# if we need to change MILESTONE
-if [ -n "$MILESTONE" ];
-then
-    TESTRAIL_MILESTONE="$MILESTONE"
-    export TESTRAIL_MILESTONE="$MILESTONE"
-fi
+TESTRAIL_URL=https://mirantis.testrail.com
+TESTRAIL_USER=releaseacceptance@mirantis.com
+TESTRAIL_PASSWORD=Release6.1
+TESTRAIL_PROJECT='Mirantis OpenStack'
+TESTRAIL_SUITE="$SUITE"
+TESTRAIL_MILESTONE="$MILESTONE"
 
 if [[ "$TESTRAIL_TEMPEST" == 'TRUE' ]] ;
 then
