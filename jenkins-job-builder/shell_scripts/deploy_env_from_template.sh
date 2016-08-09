738aec16b294326c14df4aff3e23e67934955a3c
Alexander Gromov <agromov@mirantis.com>
2016-08-09 18:25:27 +0300

    Add sri-ov_ceph job
    Change-Id: I70d6638c9f0d4a4edad5e0326fa251b3e2bd8d9e

--- jenkins-job-builder/shell_scripts/deploy_env_from_template.sh
+++ jenkins-job-builder/shell_scripts/deploy_env_from_template.sh
@@ -7,6 +7,11 @@ git checkout stable/9.0
 # change fuel-qa version to stable/mitaka
 export FUEL_QA_VER=stable/mitaka
 
+if [[ "$USE_IPMI" == 'TRUE' ]]; then
+    export MOSQA_IPMI_USER="$IPMI_USER"
+    export MOSQA_IPMI_PASSWORD="$IPMI_PASSWORD"
+fi
+
 # Not exiting from shell if error happens
 set +e
 ./deploy_template.sh $CONFIG_PATH
