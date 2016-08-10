34d6898f0e4f2218539466d4ec2daec053a9d41c
obutenko <obutenko@mirantis.com>
2016-08-10 14:07:47 +0300

    Update script for mcp team
    Change-Id: I5cce1422b00883385e640c2fa3ac135755f1dc73

--- jenkins-job-builder/shell_scripts/run_tempest_mcp.sh
+++ jenkins-job-builder/shell_scripts/run_tempest_mcp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -xe
 
 function prepare {
+    sudo rm -rf /home/vagrant/rally
     mkdir /home/vagrant/rally
     echo "sed -i 's|#swift_operator_role = Member|swift_operator_role=SwiftOperator|g' /etc/rally/rally.conf
           source /home/rally/openrc
@@ -10,24 +11,14 @@ function prepare {
           rally verify genconfig
           rally verify showconfig" > /home/vagrant/rally/install_tempest.sh
     chmod +x /home/vagrant/rally/install_tempest.sh
-    keystone_ip=$(kubectl get services --namespace demo |grep keystone |awk {'print$2'})
-    echo "export OS_NO_CACHE='true'
-          export OS_TENANT_NAME='admin'
-          export OS_PROJECT_NAME='admin'
-          export OS_USERNAME='admin'
-          export OS_PASSWORD='password'
-          export OS_AUTH_URL='http://$keystone_ip:5000/v2.0'
-          export OS_DEFAULT_DOMAIN='Default'
-          export OS_AUTH_STRATEGY='keystone'
-          export OS_REGION_NAME='RegionOne'
-          export CINDER_ENDPOINT_TYPE='internalURL'
-          export GLANCE_ENDPOINT_TYPE='internalURL'
-          export KEYSTONE_ENDPOINT_TYPE='internalURL'
-          export NOVA_ENDPOINT_TYPE='internalURL'
-          export NEUTRON_ENDPOINT_TYPE='internalURL'
-          export OS_ENDPOINT_TYPE='internalURL'
-          export MURANO_REPO_URL='http://storage.apps.openstack.org/'
-          export MURANO_PACKAGES_SERVICE='glance'" > /home/vagrant/rally/openrc
+    cp /home/vagrant/openrc-* /home/vagrant/rally/openrc
+    V2_FIX=$(cat /home/vagrant/rally/openrc |grep v2.0| wc -l)
+    if [ ${V2_FIX} == '0' ]; then
+        sed -i 's|:5000/v3|:5000/v2.0|g' /home/vagrant/rally/openrc
+    else
+        echo "openrc file already fixed"
+    fi
+    
 }
 
 function install_docker_and_run {
@@ -40,11 +31,11 @@ function install_docker_and_run {
 function run_tempest {
     source /home/vagrant/rally/openrc
     docker exec -ti $docker_id bash -c "./install_tempest.sh"
-    docker exec -ti $docker_id bash -c "source /home/rally/openrc && rally verify start $0"
+    docker exec -ti $docker_id bash -c "source /home/rally/openrc && rally verify start $1"
     docker exec -ti $docker_id bash -c "rally verify results --json --output-file result.json"
     docker exec -ti $docker_id bash -c "rally verify results --html --output-file result.html"
 }
 
 prepare
 install_docker_and_run
-run_tempest
+run_tempest "$1"
