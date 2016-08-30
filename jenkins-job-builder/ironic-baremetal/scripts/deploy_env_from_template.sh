e1a9b0e42b4dec0a3f172dc276f5b734f03987e3
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-08-30 17:16:13 +0300

    Add 2 jobs for ironic-baremets tests
    Change-Id: I37518e41a97ded2cfae9f0fbe4d0cded197ed94e

---
+++ jenkins-job-builder/ironic-baremetal/scripts/deploy_env_from_template.sh
@@ -0,0 +1,19 @@
+# This script deploy MirantisOpenStak from templates
+git clone https://github.com/Mirantis/mos-ci-deployment-scripts.git
+cd mos-ci-deployment-scripts
+git checkout stable/9.0
+
+# change fuel-qa version to stable/mitaka
+export FUEL_QA_VER=stable/mitaka
+
+# export 9.x repos
+source /home/jenkins/env_inject.properties
+export EXTRA_DEB_REPOS
+export EXTRA_RPM_REPOS
+export UPDATE_FUEL_MIRROR
+export UPDATE_MASTER
+
+# Not exiting from shell if error happens
+set +e
+./deploy_template.sh $CONFIG_PATH
+exit 0
