baaedea5e8b94e3982f4d5ce22c622825cfd52e3
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-09-01 12:59:14 +0300

    Fix deploy with ldap plugin
    Change-Id: I21af9c15281b48306432b6fdb270698a7082b943

--- test_deploy_env.py
+++ test_deploy_env.py
@@ -117,6 +117,9 @@ class DeployEnv(ActionTest, BaseActions):
                 for k, v in config_data.items():
                     configs['{0}/**/{1}/value'.format(plugin_path_prefix,
                                                       k)] = v
+            # Enable propagate_task_deploy for `ldap` plugin
+            if plugin_name == 'ldap':
+                configs['**/propagate_task_deploy/value'] = True
             logger.info("{} plugin has been enabled.".format(plugin_name))
         self._update_cluster_attributes(configs)
 
