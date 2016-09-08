ed29eabb4298acd358043592430813cfe7a0c569
Anastasia Kuznetsova <akuznetsova@mirantis.com>
2016-09-08 19:47:53 +0300

    Extend 9.0_murano_baremetal_application template and add one more job for it
    Add an ability to specify which source should be used for murano
    applications: apps.openstack.org or upstream repo.
    Based on this change one more job was added, now we will have
    two jobs run in different time (time was agreed within OPAAS/K8s team).
    
    Change-Id: I86333f6156cf99b2e909bbbb8cf8a6e5894c8e2d

--- jenkins-job-builder/shell_scripts/template_scripts/tox_based_murano_test_runner.sh
+++ jenkins-job-builder/shell_scripts/template_scripts/tox_based_murano_test_runner.sh
@@ -2,8 +2,16 @@ set +e
 
 source /home/jenkins/openrc
 
-murano bundle-import --is-public --exists-action s container-based-apps
-sleep 3m
+if [ "$APPS_FROM_CATALOG" = "TRUE" ] ; then
+    murano bundle-import --is-public --exists-action s container-based-apps
+    sleep 3m
+else
+    rm -rf k8s-docker-suite-app-murano
+
+    git clone https://github.com/openstack/k8s-docker-suite-app-murano.git
+    ./k8s-docker-suite-app-murano/tools/prepare_upload_packages.sh
+fi
+
 
 rm -rf mos-integration-tests
 git clone https://github.com/Mirantis/mos-integration-tests.git
@@ -15,6 +23,9 @@ pip install -U pip
 pip install tox
 
 printenv || true
+
+export MURANO_DOCKER_IMAGE_URL="$DOCKER_IMAGE_URL"
+export MURANO_KUBERNETES_IMAGE_URL="$K8S_IMAGE_URL"
 
 tox -r -e {tox_test_name} -- -ra -v -E "$ENV_NAME" -I "$FUEL_MASTER_IP"
 deactivate
