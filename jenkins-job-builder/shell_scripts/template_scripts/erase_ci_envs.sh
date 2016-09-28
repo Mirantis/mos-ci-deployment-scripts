cbad6f6c2e90859ac85b2b693744ff37a70f8d6d
Yury Tregubov <ytregubov@mirantis.com>
2016-09-28 18:18:06 +0400

    fix erase env
    Change-Id: I99cdb3020e2cdc9a2a652000d2080ae0730791f3

--- jenkins-job-builder/shell_scripts/template_scripts/erase_ci_envs.sh
+++ jenkins-job-builder/shell_scripts/template_scripts/erase_ci_envs.sh
@@ -3,7 +3,7 @@ PREFIX='MOS_CI_'
 echo 'Trying to erase all MOS_CI environments...'
 
 for line in $(dos.py list); do
-    if [[ "$line" == "${{PREFIX}}*${{ENV_CHANGER}}" ]]; then
+    if [[ "${{line}}" == "${{PREFIX}}"*"${{ENV_CHANGER}}" ]]; then
         echo "Erasing ${{line}}..."
         dos.py erase "$line" || true
     fi
