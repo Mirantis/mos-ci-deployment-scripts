b233d2395da61394a3a364192a073ef4a6adeaa3
obutenko <obutenko@mirantis.com>
2016-10-04 15:38:43 +0300

    Update rally_docker job
    Change slave
    Change docker image name
    
    Change-Id: I83e3b8c9072a1d91ceb98b68525a80ea9173dd0c

--- jenkins-job-builder/shell_scripts/rally_tempest_docker/rally_tempest_docker_build.sh
+++ jenkins-job-builder/shell_scripts/rally_tempest_docker/rally_tempest_docker_build.sh
@@ -2,9 +2,8 @@
 #### Remove old docker container and docker files ####
 set +e
 sudo docker rmi rally-tempest
+rm -rf dockerfiles rally_tempest_image
 set -e
-rm -rf dockerfiles
-
 git clone https://review.fuel-infra.org/fuel-infra/dockerfiles
 
 ##### Change rally version #####
@@ -12,5 +11,5 @@ sed -i 's|FROM rallyforge/rally:latest|FROM rallyforge/rally:0.6.0|g' \
        dockerfiles/rally-tempest/latest/Dockerfile
 
 #### Build and save new docker container ####
-sudo docker build -no-cache -t rally-tempest dockerfiles/rally-tempest/latest
-sudo docker save -o ./dimage rally-tempest
+sudo docker build --no-cache -t rally-tempest dockerfiles/rally-tempest/latest
+sudo docker save -o ./rally_tempest_image rally-tempest
