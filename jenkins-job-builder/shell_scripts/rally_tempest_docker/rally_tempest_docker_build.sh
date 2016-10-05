30892a4a8642a659602cc732ca95b5c37c6db2f0
obutenko <obutenko@mirantis.com>
2016-10-05 15:01:12 +0300

    Prepare scripts for new rally_tempest_docker job
    Change-Id: If68c65b7542c65048078e208d95186a7e2c31f08

--- jenkins-job-builder/shell_scripts/rally_tempest_docker/rally_tempest_docker_build.sh
+++ jenkins-job-builder/shell_scripts/rally_tempest_docker/rally_tempest_docker_build.sh
@@ -6,9 +6,13 @@ rm -rf dockerfiles rally_tempest_image
 set -e
 git clone https://review.fuel-infra.org/fuel-infra/dockerfiles
 
-##### Change rally version #####
+##### Change rally version and add option to genconfig ####
 sed -i 's|FROM rallyforge/rally:latest|FROM rallyforge/rally:0.6.0|g' \
        dockerfiles/rally-tempest/latest/Dockerfile
+
+sed -i 's|rally verify genconfig|rally verify genconfig --add-options tempest_config|g' \
+       dockerfiles/rally-tempest/latest/setup_tempest.sh
+
 
 #### Build and save new docker container ####
 sudo docker build --no-cache -t rally-tempest dockerfiles/rally-tempest/latest
