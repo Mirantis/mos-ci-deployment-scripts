a758e88449883614dce5c09bbbe1f973718765b9
obutenko <obutenko@mirantis.com>
2016-10-06 16:06:41 +0300

    Add files for new debug job
    Change-Id: Ib593b1881ee82a1ee35e89c76d24e6e2c726f750

---
+++ jenkins-job-builder/shell_scripts/debug.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -xe
+
+rm -rf rally .rally /root/openrc_tempest
+cp /root/openrc /root/openrc_tempest
+
+set +e
+source /root/openrc_tempest && ironic node-create -d fake
+set -e
+
+apt-get install -y git
+
+git clone https://github.com/openstack/rally.git
+cd rally
+IS_TLS=$(source /root/openrc_tempest; openstack endpoint show identity 2>/dev/null | awk '/https/')
+    if [ "${IS_TLS}" ]; then
+        echo "export OS_CACERT='/var/lib/astute/haproxy/public_haproxy.pem'" >> /root/openrc_tempest
+    fi
+
+sed -i "s/:5000\/'/:5000\/v3\/'/" /root/openrc_tempest
+echo "export OS_PROJECT_DOMAIN_NAME='Default'" >> /root/openrc_tempest
+echo "export OS_USER_DOMAIN_NAME='Default'" >> /root/openrc_tempest
+echo "export OS_IDENTITY_API_VERSION='3'" >> /root/openrc_tempest
+
+./install_rally.sh -d rally-venv/ -y
+
+source /root/rally/rally-venv/bin/activate
+source /root/openrc_tempest
+
+rally-manage db recreate
+rally deployment create --fromenv --name=tempest
+rally verify install --version 4db514cc0178662163e337bc0cddbdc7357c2220
+rally verify genconfig
+rally verify showconfig
+
+for i in range(20):
+    mkdir /root/run-$i
+    rally verify start --skip-list skip_ceph.list > /root/run-$i/log-$i.log
+    rally verify results --html --output-file result-$i.html
+
+deactivate
---
+++ jenkins-job-builder/shell_scripts/debug_runner.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+##### Define SSH Opts #####
+export SSH_OPTS='-o UserKnownHostsFile=/dev/null  -o StrictHostKeyChecking=no'
+
+##### Definig common job parameters #####
+ISO_NAME=$(basename "$ISO_PATH")
+ISO_ID=$(echo "$ISO_NAME" | cut -f4 -d-)
+SNAPSHOT_NAME=$(dos.py snapshot-list "$ENV_NAME" | tail -1 | awk '{print $1}')
+SNAPSHOT=$(echo $SNAPSHOT_NAME | sed 's/ha_deploy_//')
+
+##### Generate file for wrapper plugin #####
+echo "$ISO_ID"_CONF:"$SNAPSHOT" > build-name-setter.info
+
+##### Get ID of controller via SSH to admin node #####
+CONTROLLER_ID=$(echo 'fuel node | grep controller | awk '\''{print $1}'\'' | \
+                      head -1' | sshpass -p 'r00tme' ssh $SSH_OPTS -T root@"$FUEL_MASTER_IP")
+
+
+############################ add diff to keystone ########################
+wget https://raw.githubusercontent.com/Mirantis/mos-ci-deployment-scripts/master/jenkins-job-builder/shell_scripts/apply_diff
+sshpass -p 'r00tme' scp $SSH_OPTS ./apply_diff root@"$FUEL_MASTER_IP":/root/apply_diff
+sshpass -p 'r00tme' ssh $SSH_OPTS -T root@"$FUEL_MASTER_IP" "bash apply_diff"
+##########################################################################
+
+wget https://raw.githubusercontent.com/Mirantis/mos-ci-deployment-scripts/master/jenkins-job-builder/shell_scripts/debug.sh
+
+##### Copying script to master node, then to controller #####
+sshpass -p 'r00tme' scp $SSH_OPTS debug.sh root@"$FUEL_MASTER_IP":/root/debug.sh
+echo "scp /root/debug.sh node-$CONTROLLER_ID:/root/debug.sh" | sshpass -p 'r00tme' ssh $SSH_OPTS -T root@"$FUEL_MASTER_IP"
+
+##### Executing script from admin node on controller node: #####
+EXEC_CMD="echo 'bash -xe /root/debug.sh' | ssh -T node-$CONTROLLER_ID"
+echo "$EXEC_CMD" | sshpass -p 'r00tme' ssh $SSH_OPTS -T root@"$FUEL_MASTER_IP"
