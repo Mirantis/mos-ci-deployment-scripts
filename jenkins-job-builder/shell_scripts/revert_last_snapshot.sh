d15192640e7c8b1403c28122232ba01c66234c50
Yury Tregubov <ytregubov@mirantis.com>
2016-10-26 15:45:54 +0400

    add os faults config and update revert last script
    Updated revert_last_snapshot to ignore variable SNAPSHOT_NAME stated previously
    to have possibility to revert the very last snapshot even in case if deploy
    failed to finish for any reasons. Mainly this is to help in case of failed OSTF
    during deploy.
    
    And added OS_FAULTS_CONIFG variable and file generation in the stepler jobs.
    
    Change-Id: Ifd39288d97a7476c6529fb388ab9b612a9e8050e

--- jenkins-job-builder/shell_scripts/revert_last_snapshot.sh
+++ jenkins-job-builder/shell_scripts/revert_last_snapshot.sh
@@ -1,5 +1,2 @@
-if [ -z ${SNAPSHOT_NAME} ]
-then
-    SNAPSHOT_NAME=$(dos.py snapshot-list "$ENV_NAME" | tail -1 | awk '{print $1}')
-fi
+SNAPSHOT_NAME=$(dos.py snapshot-list "$ENV_NAME" | tail -1 | awk '{print $1}')
 dos.py revert-resume ${ENV_NAME} ${SNAPSHOT_NAME}
--- jenkins-job-builder/shell_scripts/template_scripts/stepler_test_runner.sh
+++ jenkins-job-builder/shell_scripts/template_scripts/stepler_test_runner.sh
@@ -7,6 +7,21 @@ pip install -r requirements.txt -r c-requirements.txt
 
 OS_AUTH_URL="${OS_AUTH_URL}v3"
 
+cat > "os_faults_cofig.yaml" << EOF
+cloud_management:
+  driver: fuel
+  args:
+    address: ${FUEL_MASTER_IP}
+    username: root
+    private_key_file: ${FUEL_KEY}
+power_management:
+  driver: libvirt
+  args:
+    connection_uri: qemu:///system
+EOF
+
+OS_FAULTS_CONFIG="${PWD}/os_faults_cofig.yaml"
+
 py.test stepler -v --ignore=stepler/horizon --junit-xml=report.xml
 deactivate
 
