5904d23baefdf717d3b5f7b7855a123106ee3214
Tregubov Yury <ytregubov@mirantis.com>
2016-10-31 19:06:26 +0300

    update of snapshot revert and os faults config
    Revert solution with forced set of last created snapshot for tempest
    tests.
    Apply the solution with revert for mos-int tests.
    And fix path to os_faults config file.
    
    Change-Id: I9df8403f414cdf93a84b064b94e973687c5e5ba4

--- jenkins-job-builder/shell_scripts/template_scripts/stepler_test_runner.sh
+++ jenkins-job-builder/shell_scripts/template_scripts/stepler_test_runner.sh
@@ -7,20 +7,20 @@ pip install -r requirements.txt -r c-requirements.txt
 
 OS_AUTH_URL="${OS_AUTH_URL}v3"
 
-cat > "os_faults_cofig.yaml" << EOF
+cat > "os_faults_config.yaml" << EOF
 cloud_management:
   driver: fuel
   args:
     address: ${FUEL_MASTER_IP}
     username: root
-    private_key_file: ${FUEL_KEY}
+    private_key_file: ${PWD}/${FUEL_KEY}
 power_management:
   driver: libvirt
   args:
     connection_uri: qemu:///system
 EOF
 
-OS_FAULTS_CONFIG="${PWD}/os_faults_cofig.yaml"
+OS_FAULTS_CONFIG="os_faults_config.yaml"
 
 py.test stepler -v --ignore=stepler/horizon --junit-xml=report.xml
 deactivate
