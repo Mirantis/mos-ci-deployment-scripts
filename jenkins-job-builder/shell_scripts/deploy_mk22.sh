b196113461df9b11bf29d99c50e2b3ebbe74e239
Evgeny Sikachev <esikachev@mirantis.com>
2016-11-17 14:51:07 +0300

    Fix mk22 job and script
    Removed name of server, env_variables and fixed image usage
    
    Change-Id: Ia3865f5fa80318490c6634651205aa976f53bc8b

--- jenkins-job-builder/shell_scripts/deploy_mk22.sh
+++ jenkins-job-builder/shell_scripts/deploy_mk22.sh
@@ -5,8 +5,11 @@ virtualenv --clear venv
 pip install -U pip
 pip install -r ./tcp_tests/requirements.txt
 
-export IMAGE_PATH1404=http://172.18.173.8:8080/jenkins/view/System%20Jobs/job/get_ubuntu_image/lastSuccessfulBuild/artifact/trusty-server-cloudimg-amd64-disk1.img
-export IMAGE_PATH1604=http://172.18.173.8:8080/jenkins/view/System%20Jobs/job/get_ubuntu_image/lastSuccessfulBuild/artifact/xenial-server-cloudimg-amd64-disk1.img 
+wget http://172.18.173.8:8080/jenkins/view/System%20Jobs/job/get_ubuntu_image/lastSuccessfulBuild/artifact/trusty-server-cloudimg-amd64-disk1.img -O trusty-server-cloudimg-amd64.qcow2
+wget http://172.18.173.8:8080/jenkins/view/System%20Jobs/job/get_ubuntu_image/lastSuccessfulBuild/artifact/xenial-server-cloudimg-amd64-disk1.img -O xenial-server-cloudimg-amd64.qcow2
+
+export IMAGE_PATH1404=trusty-server-cloudimg-amd64.qcow2
+export IMAGE_PATH1604=xenial-server-cloudimg-amd64.qcow2
 
 export ENV_NAME=tcpcloud-mk22
 export LAB_CONFIG_NAME=mk22-lab-basic
