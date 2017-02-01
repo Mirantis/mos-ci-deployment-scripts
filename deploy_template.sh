e27da2da5b5d43a5b02c7bc619dcca51518b96f8
Yury Tregubov <ytregubov@mirantis.com>
2017-02-01 12:16:03 +0400

    'Merge' env deploy from stable/9.0 to master
    Change-Id: Ie4f2664603cc73532d83f833b22c6b9d5758b03b

---
+++ deploy_template.sh
@@ -0,0 +1,143 @@
+#!/usr/bin/env bash
+
+# This script deploy MirantisOpenStak from templates
+
+
+patch_fuel_qa(){
+    # Check and apply patch to fuel_qa
+    set +e
+    pushd $PWD/fuel-qa
+    file_name=$1
+    patch_file=../fuel_qa_patches/$file_name
+    echo "Check for patch $file_name"
+    git apply --check $patch_file 2> /dev/null
+    if [ $? -eq 0 ]; then
+        echo "Applying patch $file_name"
+        git apply $patch_file
+    fi
+    popd
+    set -e
+}
+
+# exit from shell if error happens
+set -e
+
+# Hide trace on jenkins
+if [ -z "$JOB_NAME" ]; then
+    set -o xtrace
+fi
+
+if [ -z "$ISO_PATH" ]; then
+    echo "Please download ISO and define env variable ISO_PATH"
+    exit 1
+fi
+if [ ! -f "$ISO_PATH" ]; then
+    echo "$ISO_PATH is not exists or not a regular file"
+    exit 1
+fi
+
+PWD=$(pwd)
+
+CONFIG_PATH=${1:?You should pass a valid path to Yaml tempate as first argument}
+
+if [ ! -f "$CONFIG_PATH" ]; then
+    echo "$CONFIG_PATH is not exists or not a regular file"
+    exit 1
+fi
+CONFIG_FOLDER=$(basename $(dirname $CONFIG_PATH))
+CONFIG_FILE=$(basename $CONFIG_PATH)
+CONFIG_NAME="${CONFIG_FILE%.*}"
+
+SNAPSHOT_NAME=${SNAPSHOT_NAME:-$CONFIG_FOLDER"_"$CONFIG_NAME}
+
+# Set fuel QA version
+# https://github.com/openstack/fuel-qa/branches
+FUEL_QA_VER=${FUEL_QA_VER:-'master'}
+
+V_ENV_DIR="$(pwd)/fuel-devops-venv"
+
+# set ENV_NAME if it is not defined
+ENV_NAME=${ENV_NAME:-"$CONFIG_FOLDER"_"$CONFIG_NAME"_"$RANDOM"}
+DISABLE_SSL=${DISABLE_SSL:-TRUE}
+KVM_USE=${KVM_USE:-true}
+INTERFACE_MODEL=${INTERFACE_MODEL:-virtio}
+PLUGINS_CONFIG_PATH=${PLUGINS_CONFIG_PATH:-$(pwd)/plugins.yaml}
+# more time can be required to deploy env
+DEPLOYMENT_TIMEOUT=${DEPLOYMENT_TIMEOUT:-10000}
+
+export ENV_NAME DISABLE_SSL KVM_USE INTERFACE_MODEL PLUGINS_CONFIG_PATH DEPLOYMENT_TIMEOUT
+
+echo "Env name:         ${ENV_NAME}"
+echo "Snapshot name:    ${SNAPSHOT_NAME}"
+echo "Fuel QA branch:   ${FUEL_QA_VER}"
+echo ""
+
+# Check if folder for virtual env exist
+if [ ! -d "${V_ENV_DIR}" ]; then
+    virtualenv --no-site-packages ${V_ENV_DIR}
+fi
+
+source ${V_ENV_DIR}/bin/activate
+pip install -U pip
+
+# Check if fuel-qa folder exist
+if [ ! -d fuel-qa ]; then
+    git clone -b "${FUEL_QA_VER}" https://github.com/openstack/fuel-qa
+else
+    pushd fuel-qa
+    git clean -f -d -x
+    git checkout -- *
+    git checkout "${FUEL_QA_VER}"
+    git reset --hard
+    git pull
+    popd
+fi
+
+if [ ! -z "${FUEL_QA_COMMIT}" ]; then
+    pushd fuel-qa
+    git checkout ${FUEL_QA_COMMIT}
+    popd
+fi
+
+patch_fuel_qa qos.patch
+patch_fuel_qa dns_list.patch
+
+pip install -r conf/deploy_requirements.txt --upgrade
+
+pip install -r fuel-qa/fuelweb_test/requirements.txt --upgrade
+pip install -r fuel-qa/fuelweb_test/requirements-devops-source.txt --upgrade
+
+# if we want to use devops 3.0
+if [ ! -z "$FUEL_DEVOPS_VER" ]; then
+    pip install git+https://github.com/openstack/fuel-devops.git@${FUEL_DEVOPS_VER} --upgrade
+
+    # workaround for devops 3.0 installation
+    pip install psycopg2 --upgrade
+fi
+
+django-admin.py syncdb --settings=devops.settings
+django-admin.py migrate devops --settings=devops.settings
+
+cp test_deploy_env.py fuel-qa/system_test/tests/
+cp -r templates/* fuel-qa/system_test/tests_templates/
+cp $CONFIG_PATH fuel-qa/system_test/tests_templates/tests_configs/
+
+cd fuel-qa
+
+
+if [ "${INTERFACE_MODEL}" == 'virtio' ]; then
+    # Virtio network interfaces have names eth0..eth5
+    # (rather than default names - enp0s3..enp0s8)
+    for i in {0..5}; do
+        export IFACE_$i=eth$i
+    done
+fi
+
+
+# create new environment
+./run_system_test.py run 'system_test.deploy_env' --with-config $CONFIG_NAME
+
+# make snapshot if deployment is successful
+dos.py suspend ${ENV_NAME}
+dos.py snapshot ${ENV_NAME} ${SNAPSHOT_NAME}
+dos.py resume ${ENV_NAME}
---
+++ test_deploy_env.py
@@ -0,0 +1,239 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE_2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import collections
+import os
+
+import dpath.util
+
+from system_test import action
+from system_test.actions import BaseActions
+from system_test.core.discover import load_yaml
+from system_test import logger
+from system_test import testcase
+from system_test.tests import ActionTest
+
+
+@testcase(groups=['system_test.deploy_env'])
+class DeployEnv(ActionTest, BaseActions):
+    """Deploy cluster for tests
+
+    Scenario:
+        1. Create Environment
+        2. Upload plugins to the master node if it is required
+        3. Install plugins if it is required
+        4. Create cluster
+        5. Enable plugins if it is required
+        6. Add nodes to Environment
+        7. Run network checker
+        8. Deploy Environment
+        9. Run network checker
+        10. Run OSTF
+    """
+
+    base_group = ['system_test', 'system_test.deploy_env']
+    actions_order = [
+        'setup_master',
+        'config_release',
+        'make_slaves',
+        'revert_slaves',
+        'install_plugins',
+        'create_env',
+        'config_plugins',
+        'add_nodes',
+        'override_config',
+        'update_nodes',
+        'network_check',
+        'deploy_cluster',
+        'network_check',
+        'health_check',
+    ]
+
+    def __init__(self, config_file=None):
+        super(DeployEnv, self).__init__(config_file)
+        self._fuel_plugins = None
+
+        self.plugins_folder = os.environ.get("PLUGINS_PATH", '.')
+
+    @property
+    def fuel_plugins(self):
+        if self._fuel_plugins is None:
+            self._fuel_plugins = collections.OrderedDict()
+            for plugin in self.full_config.get('plugins', []):
+                for k, v in plugin.items():
+                    self._fuel_plugins[k] = v
+        return self._fuel_plugins
+
+    def _get_plugin_path(self, plugin_name):
+        files = os.listdir(self.plugins_folder)
+        variants = [x for x in files if plugin_name in x]
+        assert len(variants) == 1, (
+            "Can't find plugin file for `{0}` "
+            "within `{1}`").format(plugin_name, files)
+        plugin_filename = variants[0]
+        return os.path.join(self.plugins_folder, plugin_filename)
+
+    @action
+    def install_plugins(self):
+        """Upload and install plugins for Fuel if it is required"""
+        logger.info("The following plugins will be used: {}".format(
+            self.fuel_plugins))
+        for plugin_name in self.fuel_plugins:
+            self.plugin_path = self._get_plugin_path(plugin_name)
+            self.upload_plugin()
+            logger.info("{} plugin has been uploaded.".format(plugin_name))
+            self.install_plugin()
+            logger.info("{} plugin has been installed.".format(plugin_name))
+
+    def _get_settings_path(self, data, glob):
+        """Returns expanded path in data matched by glob"""
+        paths = [x[0] for x in dpath.util.search(data, glob, yielded=True)]
+        assert len(paths) == 1, (
+            'Should be only one path for glob `{glob}`,'
+            ' founded: {paths}').format(glob=glob, paths=paths)
+        return paths[0]
+
+    @action
+    def config_plugins(self):
+        """Config plugins for Fuel"""
+        configs = {}
+        for plugin_name, plugin_config in self.fuel_plugins.items():
+            plugin_path_prefix = '/*/{0}'.format(plugin_name)
+            # Enable plugin
+            configs['{0}/metadata/enabled'.format(plugin_path_prefix)] = True
+            if plugin_config is not None and 'config_file' in plugin_config:
+                config_data = load_yaml(plugin_config['config_file'])
+                for k, v in config_data.items():
+                    configs['{0}/**/{1}/value'.format(plugin_path_prefix,
+                                                      k)] = v
+            # Enable propagate_task_deploy for `ldap` plugin
+            if plugin_name in ('ldap', 'detach-rabbitmq'):
+                configs['**/propagate_task_deploy/value'] = True
+            logger.info("{} plugin has been enabled.".format(plugin_name))
+        self._update_cluster_attributes(configs)
+
+    def _update_dict_values(self, replacements_dict, dict_values):
+        for glob, value in replacements_dict.items():
+            path = self._get_settings_path(dict_values, glob)
+            logger.info('Set `{path}` to `{value}`'.format(path=path,
+                                                           value=value))
+            dpath.util.set(dict_values, path, value)
+        return dict_values
+
+    def _update_cluster_attributes(self, replacements_dict):
+        """Apply replacements to fuel attributes (settings)"""
+        if not replacements_dict:
+            return
+
+        attrs = self.fuel_web.client.get_cluster_attributes(self.cluster_id)
+        updated_attrs = self._update_dict_values(replacements_dict, attrs)
+
+        self.fuel_web.client.update_cluster_attributes(
+            self.cluster_id, updated_attrs)
+
+    def _update_network_params(self, replacements_dict):
+        if not replacements_dict:
+            return
+
+        params = self.fuel_web.client.get_networks(
+            cluster_id=self.cluster_id)
+
+        networking_parameters = replacements_dict.get(
+            'networking_parameters', {})
+
+        updated_net_params = self._update_dict_values(
+            replacements_dict=networking_parameters,
+            dict_values=params['networking_parameters']
+        )
+
+        self.fuel_web.client.update_network(
+            cluster_id=self.cluster_id,
+            networking_parameters=updated_net_params
+        )
+
+    @action
+    def override_config(self):
+        """Override fuel config"""
+        overrides = self.full_config.get('overrides', {})
+
+        self._update_cluster_attributes(overrides.get('attributes', {}))
+        self._update_network_params(overrides.get('networks', {}))
+
+    def _get_interface_index(self, replacements_dict, total_interface_count):
+        """Get interface number and convert it to array index"""
+        try:
+            number = replacements_dict['number']
+        except AttributeError:
+            raise AttributeError(
+                "'number' parameter should be set for each "
+                "interface which should be updated nodes."
+            )
+
+        assert 1 <= number <= total_interface_count, (
+            "'number' of interface should be from '1' to '{0}'.").format(
+                total_interface_count)
+
+        return number - 1
+
+    def _update_node_interfaces(self, node_id, replacements_list):
+        if not replacements_list:
+            return
+
+        interfaces_list = self.fuel_web.client.get_node_interfaces(
+            node_id=node_id)
+
+        for interface_repl in replacements_list:
+            interface_index = self._get_interface_index(
+                replacements_dict=interface_repl,
+                total_interface_count=len(interfaces_list)
+            )
+            interface_dict = interfaces_list[interface_index]
+
+            params_dict = interface_repl.get('params', {})
+            self._update_dict_values(replacements_dict=params_dict,
+                                     dict_values=interface_dict)
+
+        self.fuel_web.client.put_node_interfaces(
+            [{'id': node_id, 'interfaces': interfaces_list}])
+
+    def _update_node_attributes(self, node_id, replacements_dict):
+        if not replacements_dict:
+            return
+
+        attributes_dict = self.fuel_web.client.get_node_attributes(
+            node_id=node_id)
+
+        self._update_dict_values(replacements_dict=replacements_dict,
+                                 dict_values=attributes_dict)
+
+        self.fuel_web.client.upload_node_attributes(
+            attributes=attributes_dict,
+            node_id=node_id
+        )
+
+    @action
+    def update_nodes(self):
+        """Update attributes of nodes"""
+        nodes = self.full_config.get('update_nodes', [])
+
+        for node in nodes:
+            nailgun_node = self.fuel_web.get_nailgun_node_by_name(node['name'])
+
+            interfaces_list = node.get('interfaces', [])
+            self._update_node_interfaces(node_id=nailgun_node['id'],
+                                         replacements_list=interfaces_list)
+
+            attributes_dict = node.get('attributes', {})
+            self._update_node_attributes(node_id=nailgun_node['id'],
+                                         replacements_dict=attributes_dict)
