26fbc2b15f6005f701b4cb3c5039f1cf5b9dac9f
Yury Tregubov <ytregubov@mirantis.com>
2017-02-16 16:34:53 +0400

    patch mos10 flavors and docker shell refactoring
    Change-Id: I25fe235a8eb817e1bfedc7aec19553b1925e6c17

--- jenkins-job-builder/shell_scripts/template_scripts/get_fuel_key_and_openrc.sh
+++ jenkins-job-builder/shell_scripts/template_scripts/get_fuel_key_and_openrc.sh
@@ -18,4 +18,18 @@ scp -i $FUEL_KEY $SSH_OPTIONS root@$CONTROLLER_IP:openrc .
 
 . openrc
 
+if [[ $MILESTONE == 10.* ]]; then
+    virtualenv --clear venv
+    . venv/bin/activate
+    pip install git+git://github.com/openstack/python-openstackclient
+    set +e
+
+    openstack flavor create m1.tiny   --ram 512   --disk 1   --vcpus 1
+    openstack flavor create m1.small  --ram 2048  --disk 20  --vcpus 1
+    openstack flavor create m1.medium --ram 4096  --disk 40  --vcpus 2
+    openstack flavor create m1.large  --ram 8192  --disk 80  --vcpus 4
+    openstack flavor create m1.xlarge --ram 16384 --disk 160 --vcpus 8
+    deactivate
+fi
+
 env >> "${ENV_INJECT_PATH}"
