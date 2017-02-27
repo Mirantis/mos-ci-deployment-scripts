f31190822dc4e282a33233bd8a496f37b63c97a7
Tregubov Yury <ytregubov@mirantis.com>
2017-02-27 18:08:01 +0300

    disable nfv for 9.x and hack detached rabbit
    Change-Id: Ib7d6c3d6f445d2883373922c982ba4eecbd18c0f

--- jenkins-job-builder/shell_scripts/get_plugins.sh
+++ jenkins-job-builder/shell_scripts/get_plugins.sh
@@ -9,8 +9,13 @@ rm $TMPFILE
 
 # Get rabbit plugin separately
 TMPFILE=$(mktemp)
-wget https://plugin-ci.fuel-infra.org/job/9.0.fuel-plugin.detach-rabbitmq.build/lastSuccessfulBuild/artifact/*zip*/archive.zip -O $TMPFILE
-unzip -jo $TMPFILE '*.rpm' -d $PLUGINS_PATH
+#wget https://plugin-ci.fuel-infra.org/job/9.0.fuel-plugin.detach-rabbitmq.build/lastSuccessfulBuild/artifact/*zip*/archive.zip -O $TMPFILE
+#unzip -jo $TMPFILE '*.rpm' -d $PLUGINS_PATH
+
+# TBD temp solution for mos10 runs:
+
+wget https://plugin-ci.fuel-infra.org/view/rabbitmq/job/10.0.fuel-plugin.detach-rabbitmq.build/8/artifact/plugin/detach-rabbitmq-1.1-1.1.1-1.noarch.rpm -O $PLUGINS_PATH
+
 rm $TMPFILE
 
 env > "$ENV_INJECT_PATH"
